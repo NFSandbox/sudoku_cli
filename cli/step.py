ac7eb633e85f0bf51214584064da6cfa1653aecf
Averit2 <lrf2004129215@outlook.com>
2024-10-21 19:50:43 +0800

    wrong about StepCli instantiation

--- cli/step.py
+++ cli/step.py
@@ -14,7 +14,9 @@ from sudokutools.solve import bruteforce, init_candidates
 from sudokutools.analyze import find_conflicts
 from sudokutools.sudoku import Sudoku
 from .category import get_category_str
+from .sudoku import SudokuCLI
 
+from datetime import datetime
 
 from .args import *
 
@@ -22,9 +24,32 @@ from .args import *
 @with_default_category(get_category_str("Sudoku"))
 class StepCLI(cmd2.CommandSet):
 
+    def __init__(self) -> None:
+        super().__init__()
+        self.put_steps = []
+        self.start_time = datetime.now()
+
+    def log_put_execution(self, params):
+        log_entry = {
+            'time': datetime.now() - self.start_time,
+            'params': params
+        }
+        self.put_steps.append(log_entry)
+
     @with_argparser(step_parser)
     def do_step(self, args):
-        """
-        Steps history related operations
-        """
+        for s in self.put_steps:
+            rprint(s)
+            # 具体输出格式回头再写
+        pass
+
+    @with_argparser(step_show_parser)
+    def do_step_show(self, args):
+        for i in range(len(self.put_steps) - args.recent, len(self.put_steps)):
+            rprint(put_args[i])
+            # 具体输出格回头再写
+        pass
+
+    @with_argparser(step_revert_parser)
+    def do_step_revert(self, args):
         pass
