da45a773a441458523f1aece1c7f6dfa946d360b
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2024-10-22 20:56:51 +0800

    workon: diff

--- cli/step.py
+++ cli/step.py
@@ -2,6 +2,8 @@ import sys
 import os
 from datetime import datetime
 
+from pydantic import BaseModel
+
 import cmd2
 from cmd2 import Settable, Statement, with_default_category
 from cmd2 import Cmd2ArgumentParser, with_argparser, with_argument_list
@@ -19,6 +21,25 @@ from .category import get_category_str
 from .args import *
 
 
+class Diff(BaseModel):
+    time: int
+
+    # def apply()
+    # def revert()
+
+
+class DiffManager(BaseModel):
+    diff_list: list[Diff]
+
+    def __getitem__(self, index):
+        return self.diff_list[index]
+
+    # def apply(self,sudoku, by, to):
+    #     for x
+    #         diff
+    #         diff.revert(sudoku)
+
+
 @with_default_category(get_category_str("Sudoku"))
 class StepCLI(cmd2.CommandSet):
 
@@ -31,11 +52,11 @@ class StepCLI(cmd2.CommandSet):
         self._cmd: cmd2.Cmd  # for type checker like mypy
 
         # add callbacks to sudoku cli
-        sudoku_cli.put_callbacks.add("before", self.before_put_hook)
-        sudoku_cli.put_callbacks.add("after", self.after_put_hook)
+        self.sudoku_cli.put_callbacks.add("before", self.before_put_hook)
+        self.sudoku_cli.put_callbacks.add("after", self.after_put_hook)
 
-    def before_put_hook(self, *args):
-        self.put_steps.append({"before": args})
+    def before_put_hook(self, time: str, r: int, c: int, v: int, *args):
+        self.put_steps.append({"before": args[0]})
 
     def after_put_hook(self, *args):
         self._cmd.poutput(f"Received parameters: {args}")
@@ -44,7 +65,7 @@ class StepCLI(cmd2.CommandSet):
     @with_argparser(step_parser)
     def do_step(self, args):
         for i in range(len(self.put_steps)):
-            rprint(f"[blue] step{i + 1} : {self.put_steps[i]["before"][0]} ({self.put_steps[i]["before"][1]},{self.put_steps[i]["before"][2]}) {self.put_steps[i]["before"][3]}->{self.put_steps[i]["after"][3]}")
+            self._cmd.poutput(f"{self.put_steps[i].get('after', False)}")
 
     @with_argparser(step_show_parser)
     def do_step_show(self, args):
